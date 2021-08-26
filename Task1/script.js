86d7912f6ba76a6cc7ca23bebf95ad3088e3e482
shubamWanclouds <sdadhwal@wanclouds.net>
2021-08-26 19:25:24 +0530

    indentation modified

--- Task1/script.js
+++ Task1/script.js
@@ -1,19 +1,15 @@
-const makeChanges = () => {  
+const minimiseSidebar = () => {  
   let sideBar = document.getElementsByClassName("side-bar")[0];
   let companyName = document.getElementById("company-name");
   let navBtn = document.getElementById("navbtn");
-//   alert(companyName);
   
   if (sideBar.style.width === "250px") {
     sideBar.style.width = "70px";
     companyName.innerHTML="<h4>W<b>C</b></h4>";
     navBtn.style.left="100px";
-
-
   } else {
     sideBar.style.width = "250px";
     companyName.innerHTML="<h4>Wan<b>Clouds</b></h4>";
     navBtn.style.left="230px";
   }
-
 }
