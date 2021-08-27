6685220c0848db48c60e1d223e8eb567490feee5
shubamWanclouds <sdadhwal@wanclouds.net>
2021-08-27 10:26:00 +0530

    drop down added

--- Task1/script.js
+++ Task1/script.js
@@ -1,15 +1,19 @@
 const minimiseSidebar = () => {  
-  let sideBar = document.getElementsByClassName("side-bar")[0];
+  let sideBar = document.getElementsByClassName("sideBar")[0];
   let companyName = document.getElementById("company-name");
   let navBtn = document.getElementById("navbtn");
   
+  console.log(sideBar.style.width);
+  console.log(sideBar.style);
   if (sideBar.style.width === "250px") {
     sideBar.style.width = "70px";
-    companyName.innerHTML="<h4>W<b>C</b></h4>";
+    companyName.innerHTML="<h5>W<b>C</b></h5>";
+    companyName.style.marginTop="7px";
     navBtn.style.left="100px";
   } else {
     sideBar.style.width = "250px";
-    companyName.innerHTML="<h4>Wan<b>Clouds</b></h4>";
+    companyName.innerHTML="<h5>Wan<b>Clouds</b></h5>";
+    companyName.style.marginTop="7px";
     navBtn.style.left="230px";
   }
 }
